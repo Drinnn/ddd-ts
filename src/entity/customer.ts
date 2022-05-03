a392e3ff4aff5abe1fcf2d0eec70c7585247cef7
Drinnn <pedro_lontro@hotmail.com>
2022-05-02 22:10:04 -0300

    chore: add customer entity

---
+++ src/entity/customer.ts
@@ -0,0 +1,22 @@
+class Customer {
+  _id: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = true;
+
+  constructor(id: string) {
+    this._id = id;
+  }
+
+  changeName(name: string) {
+    this._name = name;
+  }
+
+  activate() {
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+}
