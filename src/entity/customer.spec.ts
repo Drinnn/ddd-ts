feeea1d4f812e22c2239580d18c5b38319dbc2df
pedro.lontro <pedro.lontro@wildlifestudios.com>
2022-05-03 08:24:21 -0300

    test(customer): add unit tests

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,60 @@
+import Address from "./address";
+import Customer from "./customer";
+
+describe("Customer unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      new Customer("", "John");
+    }).toThrowError("ID is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      new Customer("123", "");
+    }).toThrowError("Name is required");
+  });
+
+  it("should throw error when trying to change for empty name", () => {
+    const customer = new Customer("123", "John");
+
+    expect(() => {
+      customer.changeName("");
+    }).toThrowError("Name is required");
+  });
+
+  it("should change name", () => {
+    const customer = new Customer("123", "John");
+
+    customer.changeName("Doe");
+
+    expect(customer.name).toBe("Doe");
+  });
+
+  it("should throw error when trying to activate customer without address", () => {
+    const customer = new Customer("123", "John");
+    expect(() => {
+      customer.activate();
+    }).toThrow("Address is mandatory to activate a Customer");
+  });
+
+  it("should activate customer", () => {
+    const customer = new Customer("123", "John");
+    const address = new Address("Lalau St", 123, "5555-666", "Lalauland");
+    customer.address = address;
+
+    customer.activate();
+
+    expect(customer.isActive()).toBe(true);
+  });
+
+  it("should deactivate customer", () => {
+    const customer = new Customer("123", "John");
+    const address = new Address("Lalau St", 123, "5555-666", "Lalauland");
+    customer.address = address;
+
+    customer.activate();
+    customer.deactivate();
+
+    expect(customer.isActive()).toBe(false);
+  });
+});
