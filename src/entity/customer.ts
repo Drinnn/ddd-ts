4a6862dd35ca2cbe8cb4604d5676215133c2a527
Drinnn <pedro_lontro@hotmail.com>
2022-05-02 22:14:23 -0300

    feat(customer): add autovalidation

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,17 +2,34 @@ class Customer {
   _id: string;
   _name: string = "";
   _address: string = "";
-  _active: boolean = true;
+  _active: boolean = false;
 
-  constructor(id: string) {
+  constructor(id: string, name: string) {
     this._id = id;
+    this._name = name;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._id.length === 0) {
+      throw new Error("ID is required");
+    }
   }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
   activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a Customer");
+    }
+
     this._active = true;
   }
 
