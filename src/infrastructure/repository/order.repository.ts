34a50f2019a785f9a8390af257b688c170b8278d
Drinnn <pedro_lontro@hotmail.com>
2022-05-06 02:36:49 -0300

    wip(orderRepo): add order repo
    partially adds order repository

---
+++ src/infrastructure/repository/order.repository.ts
@@ -0,0 +1,37 @@
+import Order from "../../domain/entity/order";
+import RepositoryInterface from "../../domain/repository/repository-interface";
+import OrderModel from "../db/sequelize/model/order.model";
+
+export default class OrderRepository implements RepositoryInterface<Order> {
+  async create(entity: Order): Promise<void> {
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customerId: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          productId: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: ["items"],
+      }
+    );
+  }
+
+  update(entity: Order): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  find(id: string): Promise<Order> {
+    throw new Error("Method not implemented.");
+  }
+
+  findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
+  }
+}
